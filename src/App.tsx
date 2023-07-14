44b49ea849483043a5b24094127493c0150041ba
Ryan Olson <ryanolson@Ryans-MacBook-Pro.local>
2023-07-14 16:40:49 -0500

    style tweaks

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,8 @@ import React, { useEffect, useState } from "react";
 import ProductCard from "./components/Product";
 import { Product } from "./types/Product.type";
 
+window["FIT_CALCULATOR_MODAL_MODE"] = true;
+
 // Constants and configuration
 //maximum bike weight for a mototote
 const MAX_WEIGHT = 600;
@@ -164,138 +166,142 @@ function App() {
       )}
       {modalMode && modalOpen && (
         <div
-          className="modal__overlay"
+          className="fit-modal__overlay"
           onClick={() => setModalOpen(false)}
         ></div>
       )}
 
       <div
-        className={`fit-calculator-container ${modalMode ? "modal" : ""} ${
+        className={`fit-calculator-container ${modalMode ? "fit-modal" : ""} ${
           modalMode && modalOpen ? "open" : "closed"
         }`}
       >
         {modalMode && (
-          <div className="modal-header">
+          <div className="fit-modal-header">
             <h2 dangerouslySetInnerHTML={{ __html: MODAL_HEADER_TEXT }}></h2>
-            <button className="modal-close" onClick={(e) => {
-              setModalOpen(false);
-              e.preventDefault();
-            }}>
+            <button
+              className="fit-modal-close"
+              onClick={(e) => {
+                setModalOpen(false);
+                e.preventDefault();
+              }}
+            >
               &times;
             </button>
           </div>
         )}
-        <div className="formField">
-          <label>
-            <div
-              className="formLabel"
-              dangerouslySetInnerHTML={{ __html: VEHICLE_TOWING_CAPACITY }}
-            />
-            <input
-              type="number"
-              value={towCap || ""}
-              onChange={(e) => setTowCap(parseFloat(e.target.value))}
-            />
-          </label>
-        </div>
-
-        <div>
-          <label>
-            <input
-              type="checkbox"
-              value={`${aftermarket}`}
-              onChange={(e) => setAftermarket(e.target.checked)}
-            />
-            <span
-              className="formLabel"
-              dangerouslySetInnerHTML={{ __html: AFTERMARKET_HITCH }}
-            />
-          </label>
-        </div>
-
-        {aftermarket && (
+        <div className="fit-form">
           <div className="formField">
             <label>
               <div
                 className="formLabel"
-                dangerouslySetInnerHTML={{
-                  __html: HITCH_TONGUE_WEIGHT_CAPACITY,
-                }}
+                dangerouslySetInnerHTML={{ __html: VEHICLE_TOWING_CAPACITY }}
               />
               <input
                 type="number"
-                value={tongueWeight || ""}
-                onChange={(e) => setTongueWeight(parseFloat(e.target.value))}
+                value={towCap || ""}
+                onChange={(e) => setTowCap(parseFloat(e.target.value))}
               />
             </label>
           </div>
-        )}
 
-        <div className="formField">
-          <label>
-            <div
-              className="formLabel"
-              dangerouslySetInnerHTML={{ __html: BIKE_WEIGHT }}
-            />
-            <input
-              type="number"
-              value={weight || ""}
-              onChange={(e) => setWeight(parseFloat(e.target.value))}
-            />
-          </label>
-        </div>
-
-        <div className="formField">
-          <label>
-            <div
-              className="formLabel"
-              dangerouslySetInnerHTML={{ __html: FRONT_WHEEL_WIDTH }}
-            />
-            <input
-              type="number"
-              value={front || ""}
-              onChange={(e) => setFront(parseFloat(e.target.value))}
-            />
-          </label>
-        </div>
-
-        <div className="formField">
-          <label>
-            <div
-              className="formLabel"
-              dangerouslySetInnerHTML={{ __html: REAR_WHEEL_WIDTH }}
-            />
-            <input
-              type="number"
-              value={rear || ""}
-              onChange={(e) => setRear(parseFloat(e.target.value))}
-            />
-          </label>
-        </div>
-
-        {negativeMessage && (
-          <div
-            className="negativeMessage"
-            dangerouslySetInnerHTML={{ __html: negativeMessage }}
-          />
-        )}
-        {positiveMessage && (
-          <div
-            className="positiveMessage"
-            dangerouslySetInnerHTML={{ __html: positiveMessage }}
-          />
-        )}
-
-        {recommendations.length > 0 && (
-          <div className="recommendations">
-            {recommendations.map((r: string) => (
-              <ProductCard key={r} product={productInfo[r] as Product} />
-            ))}
+          <div>
+            <label>
+              <input
+                type="checkbox"
+                value={`${aftermarket}`}
+                onChange={(e) => setAftermarket(e.target.checked)}
+              />
+              <span
+                className="formLabel"
+                dangerouslySetInnerHTML={{ __html: AFTERMARKET_HITCH }}
+              />
+            </label>
           </div>
-        )}
 
+          {aftermarket && (
+            <div className="formField">
+              <label>
+                <div
+                  className="formLabel"
+                  dangerouslySetInnerHTML={{
+                    __html: HITCH_TONGUE_WEIGHT_CAPACITY,
+                  }}
+                />
+                <input
+                  type="number"
+                  value={tongueWeight || ""}
+                  onChange={(e) => setTongueWeight(parseFloat(e.target.value))}
+                />
+              </label>
+            </div>
+          )}
+
+          <div className="formField">
+            <label>
+              <div
+                className="formLabel"
+                dangerouslySetInnerHTML={{ __html: BIKE_WEIGHT }}
+              />
+              <input
+                type="number"
+                value={weight || ""}
+                onChange={(e) => setWeight(parseFloat(e.target.value))}
+              />
+            </label>
+          </div>
+
+          <div className="formField">
+            <label>
+              <div
+                className="formLabel"
+                dangerouslySetInnerHTML={{ __html: FRONT_WHEEL_WIDTH }}
+              />
+              <input
+                type="number"
+                value={front || ""}
+                onChange={(e) => setFront(parseFloat(e.target.value))}
+              />
+            </label>
+          </div>
+
+          <div className="formField">
+            <label>
+              <div
+                className="formLabel"
+                dangerouslySetInnerHTML={{ __html: REAR_WHEEL_WIDTH }}
+              />
+              <input
+                type="number"
+                value={rear || ""}
+                onChange={(e) => setRear(parseFloat(e.target.value))}
+              />
+            </label>
+          </div>
+
+          {negativeMessage && (
+            <div
+              className="negativeMessage"
+              dangerouslySetInnerHTML={{ __html: negativeMessage }}
+            />
+          )}
+          {positiveMessage && (
+            <div
+              className="positiveMessage"
+              dangerouslySetInnerHTML={{ __html: positiveMessage }}
+            />
+          )}
+
+          {recommendations.length > 0 && (
+            <div className="recommendations">
+              {recommendations.map((r: string) => (
+                <ProductCard key={r} product={productInfo[r] as Product} />
+              ))}
+            </div>
+          )}
+        </div>
         {modalMode && (
-          <div className="modal-footer">
+          <div className="fit-modal-footer">
             <button
               className="btn btn--primary btn--small"
               onClick={(e) => {
