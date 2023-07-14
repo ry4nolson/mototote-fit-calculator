27e3abe0bdf8292f03b354f9967ed189ebd7743d
Ryan Olson <ryanolson@Ryans-MacBook-Pro.local>
2023-07-14 13:12:50 -0500

    implement modal option

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,7 @@
 //ignore this please
-import React, { useEffect, useState } from 'react';
-import ProductCard from './components/Product';
-import { Product } from './types/Product.type';
-
+import React, { useEffect, useState } from "react";
+import ProductCard from "./components/Product";
+import { Product } from "./types/Product.type";
 
 // Constants and configuration
 //maximum bike weight for a mototote
@@ -22,38 +21,37 @@ const MAX_RANGE = [600, 4.75, 6.0];
 const MAX_PLUS_RANGE = [600, 5.5, 9.0];
 
 const RANGES = {
-  "m3": M3_RANGE,
-  "sport": SPORT_RANGE,
-  "max": MAX_RANGE,
-  "maxPlus": MAX_PLUS_RANGE
+  m3: M3_RANGE,
+  sport: SPORT_RANGE,
+  max: MAX_RANGE,
+  maxPlus: MAX_PLUS_RANGE,
 };
 
 const URLS = {
-  "m3" : "https://mototote.com/products/m3-motorcycle-hitch-carrier.json",
-  "sport" : "https://mototote.com/products/sport-motorcycle-hitch-carrier.json",
-  "max" : "https://mototote.com/products/max-motorcycle-carrier-1.json",
-  "maxPlus" : "https://mototote.com/products/max-plus-motorcycle-carrier.json"
+  m3: "https://mototote.com/products/m3-motorcycle-hitch-carrier.json",
+  sport: "https://mototote.com/products/sport-motorcycle-hitch-carrier.json",
+  max: "https://mototote.com/products/max-motorcycle-carrier-1.json",
+  maxPlus: "https://mototote.com/products/max-plus-motorcycle-carrier.json",
 };
 
 // Error and success messages
+const MODAL_HEADER_TEXT = "MotoTote Fit Calculator";
 const BIKE_TOO_HEAVY = "Your bike is too heavy for any MotoTote.";
-const VEHICLE_TOO_WIMPY = 
-  `Your vehicle is not rated to haul your motorcycle. 
+const VEHICLE_TOO_WIMPY = `Your vehicle is not rated to haul your motorcycle. 
   To haul your bike, you'll need a vehicle with 
   ##TOWCAP## lbs. towing capacity. You can haul a bike weighing up to 
   ##BIKEMAX## lbs. with this vehicle.`;
-const HITCH_TOO_WIMPY = 
-  `Your hitch is not rated to haul your motorcycle. 
+const HITCH_TOO_WIMPY = `Your hitch is not rated to haul your motorcycle. 
   To haul your bike, you'll need a hitch with 
   ##TOWCAP## lbs. tongue capacity. You can haul a bike weighing up to 
   ##BIKEMAX## lbs. with this hitch.`;
-const TIRES_TOO_WIDE =
-  `Sorry, your motorcycle's tires are too wide for a MotoTote. <br>
+const TIRES_TOO_WIDE = `Sorry, your motorcycle's tires are too wide for a MotoTote. <br>
   While we offer among the widest tire tracks in the industry, we currently do not 
   have a carrier that will fit your width of motorcycles tires. <br>
   We are currently developing a new solution for wider tires.  <br>
   For more help, click the red chat icon to the right.`;
-const RECOMMENDATIONS = `For your motorcycle, we recommend: `
+const RECOMMENDATIONS = `For your motorcycle, we recommend: `;
+const MODAL_BUTTON_TEXT = "Fit Calculator";
 
 // Page text and labels
 const VEHICLE_TOWING_CAPACITY = "Vehicle Total Towing Capacity (lbs.)";
@@ -62,9 +60,6 @@ const HITCH_TONGUE_WEIGHT_CAPACITY = "Hitch Tongue Weight Capacity";
 const BIKE_WEIGHT = "Bike Weight";
 const FRONT_WHEEL_WIDTH = "Front Wheel Width";
 const REAR_WHEEL_WIDTH = "Rear Wheel Width";
-
-
-
 
 // DANGER ZONE
 // DANGER ZONE
@@ -85,11 +80,20 @@ function App() {
   const [negativeMessage, setNegativeMessage] = useState<string>("");
   const [positiveMessage, setPositiveMessage] = useState<string>("");
   const [recommendations, setRecommendations] = useState<string[]>([]);
-  const [productInfo, setProductInfo] = useState<{ [key : string] : Product}>({});
+  const [productInfo, setProductInfo] = useState<{ [key: string]: Product }>(
+    {}
+  );
+  const [modalOpen, setModalOpen] = useState<boolean>(false);
 
-  const weightCapacity = aftermarket ?
-    tongueWeight < towCap ? tongueWeight : 0 :
-    towCap * 0.1
+  const modalMode: boolean =
+    "FIT_CALCULATOR_MODAL_MODE" in window &&
+    window["FIT_CALCULATOR_MODAL_MODE"] === true;
+
+  const weightCapacity = aftermarket
+    ? tongueWeight < towCap
+      ? tongueWeight
+      : 0
+    : towCap * 0.1;
 
   useEffect(() => {
     (async () => {
@@ -101,11 +105,9 @@ function App() {
         return [key, data.product];
       });
 
-
       const data = await Promise.all(promises);
       const productInfo = Object.fromEntries(data);
       setProductInfo(productInfo);
-
     })();
   }, []);
 
@@ -120,8 +122,16 @@ function App() {
       setNegativeMessage(BIKE_TOO_HEAVY);
     } else if (weight + WEIGHT_BUFFER > weightCapacity) {
       let weightMessage = aftermarket ? HITCH_TOO_WIMPY : VEHICLE_TOO_WIMPY;
-      weightMessage = weightMessage.replace("##TOWCAP##", `${aftermarket ? (weight + WEIGHT_BUFFER) : (weight + WEIGHT_BUFFER) * 10}`);
-      weightMessage = weightMessage.replace("##BIKEMAX##", `${weightCapacity - WEIGHT_BUFFER}`);
+      weightMessage = weightMessage.replace(
+        "##TOWCAP##",
+        `${
+          aftermarket ? weight + WEIGHT_BUFFER : (weight + WEIGHT_BUFFER) * 10
+        }`
+      );
+      weightMessage = weightMessage.replace(
+        "##BIKEMAX##",
+        `${weightCapacity - WEIGHT_BUFFER}`
+      );
       setNegativeMessage(weightMessage);
     } else if (front > MAX_FRONT || rear > MAX_REAR) {
       setNegativeMessage(TIRES_TOO_WIDE);
@@ -129,66 +139,166 @@ function App() {
 
     setPositiveMessage(RECOMMENDATIONS);
 
-    const recommendations : string[] = [];
-    for (const [key, [max_weight, max_front, max_rear]] of Object.entries(RANGES)) {
+    const recommendations: string[] = [];
+    for (const [key, [max_weight, max_front, max_rear]] of Object.entries(
+      RANGES
+    )) {
       if (weight > max_weight) continue;
       if (front > max_front || rear > max_rear) continue;
       recommendations.push(key);
     }
     setRecommendations(recommendations);
-
   }, [towCap, aftermarket, tongueWeight, weight, front, rear, weightCapacity]);
 
   return (
     <div className="fit-calculator">
-      <div className="formField">
-        <label>
-          <div className="formLabel" dangerouslySetInnerHTML={{ __html: VEHICLE_TOWING_CAPACITY }} />
-          <input type="number" value={towCap || ""} onChange={e => setTowCap(parseFloat(e.target.value))} />
-        </label>
+      {modalMode && (
+        <button
+          className="slide__btn btn btn--primary btn--small"
+          onClick={() => setModalOpen(true)}
+          dangerouslySetInnerHTML={{ __html: MODAL_BUTTON_TEXT }}
+        />
+      )}
+      {modalMode && modalOpen && (
+        <div
+          className="modal__overlay"
+          onClick={() => setModalOpen(false)}
+        ></div>
+      )}
+
+      <div
+        className={`fit-calculator-container ${modalMode ? "modal" : ""} ${
+          modalMode && modalOpen ? "open" : "closed"
+        }`}
+      >
+        {modalMode && (
+          <div className="modal-header">
+            <h2 dangerouslySetInnerHTML={{ __html: MODAL_HEADER_TEXT }}></h2>
+            <button className="modal-close" onClick={() => setModalOpen(false)}>
+              &times;
+            </button>
+          </div>
+        )}
+        <div className="formField">
+          <label>
+            <div
+              className="formLabel"
+              dangerouslySetInnerHTML={{ __html: VEHICLE_TOWING_CAPACITY }}
+            />
+            <input
+              type="number"
+              value={towCap || ""}
+              onChange={(e) => setTowCap(parseFloat(e.target.value))}
+            />
+          </label>
+        </div>
+
+        <div>
+          <label>
+            <input
+              type="checkbox"
+              value={`${aftermarket}`}
+              onChange={(e) => setAftermarket(e.target.checked)}
+            />
+            <span
+              className="formLabel"
+              dangerouslySetInnerHTML={{ __html: AFTERMARKET_HITCH }}
+            />
+          </label>
+        </div>
+
+        {aftermarket && (
+          <div className="formField">
+            <label>
+              <div
+                className="formLabel"
+                dangerouslySetInnerHTML={{
+                  __html: HITCH_TONGUE_WEIGHT_CAPACITY,
+                }}
+              />
+              <input
+                type="number"
+                value={tongueWeight || ""}
+                onChange={(e) => setTongueWeight(parseFloat(e.target.value))}
+              />
+            </label>
+          </div>
+        )}
+
+        <div className="formField">
+          <label>
+            <div
+              className="formLabel"
+              dangerouslySetInnerHTML={{ __html: BIKE_WEIGHT }}
+            />
+            <input
+              type="number"
+              value={weight || ""}
+              onChange={(e) => setWeight(parseFloat(e.target.value))}
+            />
+          </label>
+        </div>
+
+        <div className="formField">
+          <label>
+            <div
+              className="formLabel"
+              dangerouslySetInnerHTML={{ __html: FRONT_WHEEL_WIDTH }}
+            />
+            <input
+              type="number"
+              value={front || ""}
+              onChange={(e) => setFront(parseFloat(e.target.value))}
+            />
+          </label>
+        </div>
+
+        <div className="formField">
+          <label>
+            <div
+              className="formLabel"
+              dangerouslySetInnerHTML={{ __html: REAR_WHEEL_WIDTH }}
+            />
+            <input
+              type="number"
+              value={rear || ""}
+              onChange={(e) => setRear(parseFloat(e.target.value))}
+            />
+          </label>
+        </div>
+
+        {negativeMessage && (
+          <div
+            className="negativeMessage"
+            dangerouslySetInnerHTML={{ __html: negativeMessage }}
+          />
+        )}
+        {positiveMessage && (
+          <div
+            className="positiveMessage"
+            dangerouslySetInnerHTML={{ __html: positiveMessage }}
+          />
+        )}
+
+        {recommendations.length > 0 && (
+          <div className="recommendations">
+            {recommendations.map((r: string) => (
+              <ProductCard key={r} product={productInfo[r] as Product} />
+            ))}
+          </div>
+        )}
+
+        {modalMode && (
+          <div className="modal-footer">
+            <button
+              className="btn btn--primary btn--small"
+              onClick={() => setModalOpen(false)}
+            >
+              Close
+            </button>
+          </div>
+        )}
       </div>
-
-      <div>
-        <label>
-          <input type="checkbox" value={`${aftermarket}`} onChange={e => setAftermarket(e.target.checked)} />
-          <span className="formLabel" dangerouslySetInnerHTML={{ __html: AFTERMARKET_HITCH }} />
-        </label>
-      </div>
-
-      {aftermarket && <div className="formField">
-        <label>
-          <div className="formLabel" dangerouslySetInnerHTML={{ __html: HITCH_TONGUE_WEIGHT_CAPACITY }} />
-          <input type="number" value={tongueWeight || ""} onChange={e => setTongueWeight(parseFloat(e.target.value))} />
-        </label>
-      </div>}
-
-      <div className="formField">
-        <label>
-          <div className="formLabel" dangerouslySetInnerHTML={{ __html: BIKE_WEIGHT }} />
-          <input type="number" value={weight || ""} onChange={e => setWeight(parseFloat(e.target.value))} />
-        </label>
-      </div>
-
-      <div className="formField">
-        <label>
-          <div className="formLabel" dangerouslySetInnerHTML={{ __html: FRONT_WHEEL_WIDTH }} />
-          <input type="number" value={front || ""} onChange={e => setFront(parseFloat(e.target.value))} />
-        </label>
-      </div>
-
-      <div className="formField">
-        <label>
-          <div className="formLabel" dangerouslySetInnerHTML={{ __html: REAR_WHEEL_WIDTH }} />
-          <input type="number" value={rear || ""} onChange={e => setRear(parseFloat(e.target.value))} />
-        </label>
-      </div>
-
-      {negativeMessage && <div className="negativeMessage" dangerouslySetInnerHTML={{ __html: negativeMessage }} />}
-      {positiveMessage && <div className="positiveMessage" dangerouslySetInnerHTML={{ __html: positiveMessage }} />}
-
-      {recommendations.length > 0 && <div className="recommendations">
-        {recommendations.map((r : string) => <ProductCard key={r} product={productInfo[r] as Product} />)}
-      </div>}
     </div>
   );
 }
--- src/index.tsx
+++ src/index.tsx
@@ -1,45 +1,13 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
-import './index.css?inline';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+const css = (await import("./index.css?raw")).default;
 
 /* eslint-disable import/no-webpack-loader-syntax */
-const root = ReactDOM.createRoot(document.getElementById('fit-calculator')!);
+const root = ReactDOM.createRoot(document.getElementById("fit-calculator")!);
 root.render(
   <React.StrictMode>
-    <style dangerouslySetInnerHTML={{ __html: `
-.fit-calculator * {
-  box-sizing: border-box;
-}
-.fit-calculator {
-  max-width: 600px;
-  margin: 0 auto;
-}
-.fit-calculator .formField {
-  padding:0.5rem 0;
-}
-.fit-calculator .formLabel {
-  font-weight:600;
-  margin-bottom:0.25rem;
-}
-.fit-calculator .formField input:not([type="checkbox"]) {
-  width:100%;
-}
-.fit-calculator input[type=number]::-webkit-inner-spin-button,
-.fit-calculator input[type=number]::-webkit-outer-spin-button {
-  opacity: 1;
-  height:40px;
-  -webkit-appearance: auto;
-}
-.fit-calculator .recommendations {
-  display: flex;
-  flex-wrap: wrap;
-  justify-content: space-between;
-}
-.fit-calculator .product {
-  width:200px;
-}
-    `}} />
+    <style dangerouslySetInnerHTML={{ __html: css }} />
     <App />
   </React.StrictMode>
 );
