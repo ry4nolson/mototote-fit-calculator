0267fc2a15a6a4cc6de931d63471f887302d8b19
Ryan Olson <ry4nolson@gmail.com>
2023-07-20 17:04:31 -0500

    add classes, refactor css

--- src/components/Product.tsx
+++ src/components/Product.tsx
@@ -4,12 +4,14 @@ import { Product } from '../types/Product.type';
 export default function ProductCard({product}: {product: Product}) {
 
   return (
-    <a className="product" href={`/products/${product.handle}`}>
-      <div>
-        <div className="productImage">
+    <a className="product product-grid-item" href={`/products/${product.handle}`}>
+      <div className="product-grid-item__inner">
+        <div className="productImage product-grid-item__image">
           <img src={`${product.image.src}&width=200`} alt={product.title} />
         </div>
+        <div className="custom_item__info">
         <h3>{product.title}</h3>
+        </div>
       </div>
     </a>
   );
--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,19 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
-const css = (await import("./index.css?raw")).default;
+const index = (await import("./css/index.css?raw")).default;
+const modal = (await import("./css/modal.css?raw")).default;
+const results = (await import("./css/results.css?raw")).default;
+const form = (await import("./css/form.css?raw")).default;
 
 /* eslint-disable import/no-webpack-loader-syntax */
 const root = ReactDOM.createRoot(document.getElementById("fit-calculator")!);
 root.render(
   <React.StrictMode>
-    <style dangerouslySetInnerHTML={{ __html: css }} />
+    <style dangerouslySetInnerHTML={{ __html: index }} />
+    <style dangerouslySetInnerHTML={{ __html: form }} />
+    <style dangerouslySetInnerHTML={{ __html: modal }} />
+    <style dangerouslySetInnerHTML={{ __html: results }} />
     <App />
   </React.StrictMode>
 );
